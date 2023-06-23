230f1b672df4fd132e353660e1b1a84d327228bc
Kalips2 <100539578+Kalips2@users.noreply.github.com>
2023-06-23 20:22:30 +0300

    Refactoring due to mentor comments.

--- repository/email_repository.go
+++ repository/email_repository.go
@@ -1,85 +1,72 @@
 package repository
 
 import (
-	"encoding/csv"
+	"encoding/json"
 	"github.com/pkg/errors"
 	"os"
 )
 
 var (
-	failToSaveEmailMessage = "Failed to subscribe email"
-	failToGetEmailsMessage = "Failed to get emails from storage"
-	permToOpenTheStorage   = 0644
+	failToSubscribeEmailMessage = "Failed to subscribe email"
+	failToReadStorageMessage    = "Failed to read from storage"
+	permToOpenTheStorage        = 0644
 )
 
 func SaveEmailToStorage(email string, pathToStorage string) error {
 	var err error
-	var file *os.File
-	defer func(file *os.File) {
-		err = file.Close()
-	}(file)
 
-	file, err = setUpConnectionWithStorage(pathToStorage)
+	err = writeToStorage(email, pathToStorage)
 	if err != nil {
-		return errors.Wrap(err, failToSaveEmailMessage)
-	}
-
-	err = writeToStorage(email, file)
-	if err != nil {
-		return errors.Wrap(err, failToSaveEmailMessage)
+		return errors.Wrap(err, failToSubscribeEmailMessage)
 	}
 	return err
 }
 
 func GetEmailsFromStorage(pathToStorage string) ([]string, error) {
-	var file *os.File
 	var err error
 	var emails []string
 
-	file, err = setUpConnectionWithStorage(pathToStorage)
+	emails, err = readFromStorage(pathToStorage)
 	if err != nil {
-		return nil, errors.Wrap(err, failToGetEmailsMessage)
-	}
-
-	emails, err = readFromStorage(file)
-	if err != nil {
-		return nil, errors.Wrap(err, failToGetEmailsMessage)
+		return nil, errors.Wrap(err, failToReadStorageMessage)
 	}
 	return emails, err
 }
 
-func writeToStorage(email string, storage *os.File) error {
-	writer := csv.NewWriter(storage)
-	defer writer.Flush()
-	err := writer.Write([]string{email})
-
+func writeToStorage(email string, pathToStorage string) error {
+	var err error
+	emails, err := readFromStorage(pathToStorage)
 	if err != nil {
-		return errors.New("Failed to write into storage")
+		return err
 	}
+	emails = append(emails, email)
+
+	data, err := json.Marshal(emails)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(pathToStorage, data, os.FileMode(permToOpenTheStorage))
+	if err != nil {
+		return err
+	}
+
 	return err
 }
 
-func readFromStorage(storage *os.File) ([]string, error) {
-	var data []string
-	reader := csv.NewReader(storage)
-	records, err := reader.ReadAll()
-
+func readFromStorage(pathToStorage string) ([]string, error) {
+	data, err := os.ReadFile(pathToStorage)
 	if err != nil {
-		return nil, errors.New("Failed to read from storage")
+		return nil, errors.New(failToReadStorageMessage)
 	}
 
-	for _, record := range records {
-		data = append(data, record[0])
-	}
-	return data, err
-}
-
-func setUpConnectionWithStorage(pathToStorage string) (*os.File, error) {
-	storage, err := os.OpenFile(pathToStorage, os.O_RDWR|os.O_APPEND|os.O_CREATE, os.FileMode(permToOpenTheStorage))
+	var emails []string
+	err = json.Unmarshal(data, &emails)
 	if err != nil {
-		return nil, errors.New("Failed to set up connection with storage")
+		return nil, err
 	}
-	return storage, err
+
+	return emails, nil
 }
 
 func CheckEmailIsExist(email string, pathToStorage string) (bool, error) {
