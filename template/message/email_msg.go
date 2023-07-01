253124b46355a7aac1533aad6cd31e78b6d5104f
Kalips2 <100539578+Kalips2@users.noreply.github.com>
2023-07-01 18:14:22 +0300

    Fix MR conflicts.

--- template/message/email_msg.go
+++ template/message/email_msg.go
@@ -4,4 +4,5 @@ var (
 	EmailSubscribed        = "Emails have been subscribed."
 	EmailsWereSent         = "Emails have been sent."
 	FailToSendRateMessage  = "Failed to send the rate to emails"
+	FailToSubscribeMessage = "Failed to subscribe email"
 )
