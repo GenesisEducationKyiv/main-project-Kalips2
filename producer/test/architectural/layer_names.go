69be18f63ff903492737a0946075f5dcca9c23a3
Kalips2 <100539578+Kalips2@users.noreply.github.com>
2023-07-17 19:24:46 +0300

    Update architectural test.

--- producer/test/architectural/layer_names.go
+++ producer/test/architectural/layer_names.go
@@ -3,6 +3,6 @@ package architectural
 var (
 	domainLayer         = []string{domain}
 	applicationLayer    = []string{application}
-	infrastructureLayer = []string{provider, repository, sender}
+	infrastructureLayer = []string{provider, repository, sender, logger}
 	presenterLayer      = []string{handler, presenter}
 )
---
+++ producer/test/architectural/package_names.go
@@ -0,0 +1,12 @@
+package architectural
+
+var (
+	domain      = "producer/pkg/domain/..."
+	application = "producer/pkg/application/..."
+	handler     = "producer/pkg/presentation/handler/..."
+	presenter   = "producer/pkg/presentation/presenter/..."
+	provider    = "producer/pkg/infrastructure/provider/..."
+	repository  = "producer/pkg/infrastructure/repository/..."
+	sender      = "producer/pkg/infrastructure/sender/..."
+	logger      = "producer/pkg/infrastructure/logger/..."
+)
